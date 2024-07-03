7975845761de524bc9b2882e3364e9e3b97bd201
Cory Daddona <corydaddona1@hotmail.com>
2024-07-03 18:38:31 -0500

    added ai to app

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-// import OpenAI from "openai"
+import OpenAI from 'openai'
 
 const translateBtn = document.querySelector('.translate-btn')
 
@@ -7,7 +7,35 @@ translateBtn.addEventListener('click', translateText)
 
 async function translateText() {
   const textToTranslate = document.getElementById('textarea').value
-  const selectedLanguage = document.querySelector('input[name=language]:checked').value;
+  const selectedLanguage = document.querySelector('input[name=language]:checked').value
 
-  console.log(textToTranslate, selectedLanguage)
+  const messages = [
+    {
+      role: 'system',
+      content: `You can translate english text to ${selectedLanguage} like an expert.`
+    },
+    {
+      role: 'user',
+      content: `${textToTranslate}`
+    }
+  ]
+
+  try {
+    const openai = new OpenAI({
+      dangerouslyAllowBrowser: true,
+      apiKey: import.meta.env.VITE_OPENAI_API_KEY
+    })
+  
+    const response = await openai.chat.completions.create({
+      model: "gpt-4",
+      messages: messages,
+      temperature: 1,
+      max_tokens: 256,
+    })
+
+    console.log(response.choices[0].message.content)
+
+  } catch (err) {
+      console.log(err)
+  }
 }
