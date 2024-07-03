db07ed58a7152b586d2a51d8def409394e0cd2cf
Cory Daddona <corydaddona1@hotmail.com>
2024-07-03 15:23:21 -0500

    formated html and started js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './style.css'
+// import OpenAI from "openai"
+
+const translateBtn = document.querySelector('.translate-btn')
+
+translateBtn.addEventListener('click', translateText)
+
+async function translateText() {
+  const textToTranslate = document.getElementById('textarea').value
+  const selectedLanguage = document.querySelector('input[name=language]:checked').value;
+
+  console.log(textToTranslate, selectedLanguage)
+}
