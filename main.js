b4872900ed1b8ea6782db858203ec5078f089a01
Cory Daddona <corydaddona1@hotmail.com>
2024-07-08 19:24:29 -0500

    added sections and render text

--- main.js
+++ main.js
@@ -33,9 +33,24 @@ async function translateText() {
       max_tokens: 256,
     })
 
-    console.log(response.choices[0].message.content)
+    renderText(response.choices[0].message.content)
 
   } catch (err) {
       console.log(err)
   }
+}
+
+function renderText(text) {
+  const languagesContainer = document.querySelector('.languages')
+  languagesContainer.style.display = 'none'
+  document.getElementById('translate-title').textContent = ('Original text 👇')
+  document.getElementById('language-title').textContent = ('Your translation 👇')
+  translateBtn.textContent = ('Start Over')
+  const outputArea = document.querySelector('.output-section')
+  const outputText = document.createElement('textarea')
+  outputArea.appendChild(outputText)
+  outputText.textContent = text
+  outputText.classList.add('section-textarea')
+  document.querySelector('.section-textarea').disabled = true
+  outputText.disabled = true
 }
