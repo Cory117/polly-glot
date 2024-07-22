33915aebbd910f3705e62b857be416031c947545
Cory Daddona <corydaddona1@hotmail.com>
2024-07-22 16:23:25 -0500

    updated js

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 import './style.css'
 import OpenAI from 'openai'
 
+// Variables
 const translateBtn = document.querySelector('.translate-btn')
+const restartBtn = document.querySelector('.restart-btn')
+const inputPage = document.querySelector('.input-page')
+const inputText = document.getElementById('input-text')
+const languageRadios = document.getElementsByName('language')
+const outputPage = document.querySelector('.output-page')
+const outputOriginalText = document.getElementById('output-original-text')
+const outputTranslatedText = document.getElementById('output-translated-text')
 
+// Event Listeners 
 translateBtn.addEventListener('click', translateText)
+restartBtn.addEventListener('click', startOver)
 
+// Functions
 async function translateText() {
-  const textToTranslate = document.getElementById('textarea').value
-  const selectedLanguage = document.querySelector('input[name=language]:checked').value
+  const textToTranslate = inputText.value
+  let selectedLanguage
+
+  for (var i = 0, length = languageRadios.length; i < length; i++) {
+    if (languageRadios[i].checked) {
+      selectedLanguage = languageRadios[i].value
+      break
+    }
+  }
 
   const messages = [
     {
@@ -20,37 +38,42 @@ async function translateText() {
     }
   ]
 
-  try {
-    const openai = new OpenAI({
-      dangerouslyAllowBrowser: true,
-      apiKey: import.meta.env.VITE_OPENAI_API_KEY
-    })
+  if (textToTranslate && selectedLanguage) {
+    try {
+      const openai = new OpenAI({
+        dangerouslyAllowBrowser: true,
+        apiKey: import.meta.env.VITE_OPENAI_API_KEY
+      })
+    
+      const response = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: messages,
+        temperature: 1,
+        max_tokens: 256,
+      })
   
-    const response = await openai.chat.completions.create({
-      model: "gpt-4",
-      messages: messages,
-      temperature: 1,
-      max_tokens: 256,
-    })
-
-    renderText(response.choices[0].message.content)
-
-  } catch (err) {
-      console.log(err)
+      renderText(response.choices[0].message.content)
+  
+    } catch (err) {
+        console.log(err)
+    }
+  } else {
+    alert('Please enter text and select a language.')
   }
 }
 
 function renderText(text) {
-  const languagesContainer = document.querySelector('.languages')
-  languagesContainer.style.display = 'none'
-  document.getElementById('translate-title').textContent = ('Original text 👇')
-  document.getElementById('language-title').textContent = ('Your translation 👇')
-  translateBtn.textContent = ('Start Over')
-  const outputArea = document.querySelector('.output-section')
-  const outputText = document.createElement('textarea')
-  outputArea.appendChild(outputText)
-  outputText.textContent = text
-  outputText.classList.add('section-textarea')
-  document.querySelector('.section-textarea').disabled = true
-  outputText.disabled = true
+  inputPage.style.display = 'none'
+  outputPage.style.display = 'inline'
+  outputOriginalText.textContent = inputText.value
+  outputOriginalText.disabled = true
+  outputTranslatedText.textContent = text
+  outputTranslatedText.disabled = true
+}
+
+function startOver() {
+  outputPage.style.display = 'none'
+  inputPage.style.display = 'inline'
+  inputText.value = ''
+  languageRadios.forEach(radio => radio.checked = false)
 }
